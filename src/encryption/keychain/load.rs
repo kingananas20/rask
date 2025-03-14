f6b976751d55fe6ff4bbc1b8db98f52912799231
kingananas20 <jonasarter@outlook.com>
2025-03-14 13:30:09 +0100

    added keychain functions

--- src/encryption/keychain/load.rs
+++ src/encryption/keychain/load.rs
@@ -1,5 +1,20 @@
-use keyring;
+use keyring::Entry;
+use anyhow::Result;
 
-pub fn create() {}
-pub fn read() {}
-pub fn write() {}
+pub fn read(service: &str, user: &str) -> Result<Vec<u8>> {
+    let entry: Entry = Entry::new(service, user)?;
+    let secret: Vec<u8> = entry.get_secret()?;
+    Ok(secret)
+}
+
+pub fn write(service: &str, user: &str, mut secret: Vec<u8>) -> Result<()> {
+    let entry: Entry = Entry::new(service, user)?;
+    entry.set_secret(&secret)?;
+    secret.fill(0);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
