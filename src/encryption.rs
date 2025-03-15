1140b838f2bdf072adfadd97c668b1db28039221
kingananas20 <jonasarter@outlook.com>
2025-03-15 17:45:05 +0100

    added more context to keychain

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,1 +1,2 @@
 mod keychain;
+mod generatekey;
--- src/encryption/keychain/load.rs
+++ src/encryption/keychain/load.rs
@@ -1,15 +1,19 @@
 use keyring::Entry;
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 pub fn read(service: &str, user: &str) -> Result<Vec<u8>> {
-    let entry: Entry = Entry::new(service, user)?;
-    let secret: Vec<u8> = entry.get_secret()?;
+    let entry: Entry = Entry::new(service, user)
+    .with_context(|| format!("error creating entry with service: {} and user: {}", service, user))?;
+    let secret: Vec<u8> = entry.get_secret()
+        .context("error getting secret")?;
     Ok(secret)
 }
 
 pub fn write(service: &str, user: &str, mut secret: Vec<u8>) -> Result<()> {
-    let entry: Entry = Entry::new(service, user)?;
-    entry.set_secret(&secret)?;
+    let entry: Entry = Entry::new(service, user)
+        .with_context(|| format!("error creating entry with service: {} and user: {}", service, user))?;
+    entry.set_secret(&secret)
+        .context("error setting secret")?;
     secret.fill(0);
     Ok(())
 }
